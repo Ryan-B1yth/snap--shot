027ed80195992654d2151037e15e2fc4a18220fe
Ryan Blyth <ryan.blyth@hotmail.com>
2022-06-13 18:50:27 +0000

    Add home app and wire up template dir

---
+++ home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    """ Render home page """
+    return render(request, 'home/index.html', {})
--- snap_shot/settings.py
+++ snap_shot/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -44,10 +45,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'snap_shot.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
