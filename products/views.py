3ed208abb0d44d8940b6f68af4b542f01160f653
Ryan Blyth <ryan.blyth@hotmail.com>
2022-06-14 18:56:55 +0000

    Add products page and initial styling

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def all_products(request):
+    """ All products view, product search handling """
+
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context)
