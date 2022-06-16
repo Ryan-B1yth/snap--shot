0e194b27a75f69ed0eb4a1e6ce1d58871d591d36
Ryan Blyth <ryan.blyth@hotmail.com>
2022-06-16 20:03:31 +0000

    Add and start styling product detail page

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import all_products
+from .views import all_products, product_detail
 
 urlpatterns = [
-    path('', all_products, name='products')
+    path('', all_products, name='products'),
+    path('<product_id>', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
+
 
 def all_products(request):
     """ All products view, product search handling """
@@ -11,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ Product detail view """
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
