4a5e332c1c4ddef2f458d989fdcb4e1235fc01ec
Ryan Blyth <ryan.blyth@hotmail.com>
2022-06-18 12:04:53 +0000

    Add js functionality to sort selector box

---
+++ static/js/products.js
@@ -0,0 +1,21 @@
+$('#sort-selector').change( function() {
+    let selector = $(this);
+    currentUrl = new URL(window.location);
+
+    selectedValue = selector.val()
+    if (selectedValue != 'reset') {
+        var sort = selectedValue.split('_')[0];
+        var direction = selectedValue.split('_')[1];
+
+        currentUrl.searchParams.set('sort', sort);
+        currentUrl.searchParams.set('direction', direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete('sort');
+        currentUrl.searchParams.delete('direction');
+
+        window.location.replace(currentUrl);
+
+    }
+})
