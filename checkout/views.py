49df49930772addc31991706bdb3881c2827a166
Ryan Blyth <ryan.blyth@hotmail.com>
2022-07-05 21:19:44 +0000

    Add Stripe card to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,7 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IQV5IEl9zAfsjE63w7v6gyHVgLuxqRhieQIxStB3TF5EVKsm1ns9ofNHGh8E4OfHcCMqZxfZhk2gOKv5YvUQEvj00SUn4dkGb',
     }
 
     return render(request, 'checkout/checkout.html', context)
