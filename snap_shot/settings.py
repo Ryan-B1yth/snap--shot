c52fda1bc0cf2f7be506f070d57eda153f814955
Ryan Blyth <ryan.blyth@hotmail.com>
2022-07-14 19:56:12 +0000

    Prepare to deploy to heroku

--- snap_shot/settings.py
+++ snap_shot/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-r5ab4nch=nrbil1y!9r4_=rma=t&wyx#o8z8icz(w!qdm1xd^o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snap-shot-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -97,16 +97,21 @@ WSGI_APPLICATION = 'snap_shot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
