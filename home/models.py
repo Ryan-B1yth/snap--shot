81dfe37e92bff5889d94f55225feb485c3a5f56a
Ryan Blyth <ryan.blyth@hotmail.com>
2022-07-28 18:42:26 +0000

    Add testimony model, not wired up yet

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Testimony(models.Model):
+    """ Testimony model """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=500, null=False, blank=False)
