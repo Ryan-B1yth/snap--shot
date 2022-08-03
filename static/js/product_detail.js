0212bcc0d142501f7004326d40bc6c0e6dcfed49
Ryan Blyth <ryan.blyth@hotmail.com>
2022-08-03 09:23:42 +0000

    Fix basket quantity change bug

--- static/js/product_detail.js
+++ static/js/product_detail.js
@@ -1,17 +1,23 @@
-$('#increment').click( function(e) {
-    e.preventDefault();
-    var value = parseInt($('#quantity').val());
+$('.increment').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue + 1);
+   var itemId = $(this).data('item_id');
 
-    isNaN(value) || value >= 99 ? 
-        $('#quantity').val(99) : 
-        $('#quantity').val(value + 1);
+    isNaN(currentValue) || currentValue >= 99 ? 
+        $('.quantity').val(99) : 
+        $('.quantity').val(currentValue + 1);
 });
 
-$('#decrement').click( function(e) {
-    e.preventDefault();
-    var value = parseInt($('#quantity').val());
-    
-    isNaN(value) || value <= 0 ? 
-        $('#quantity').val(0) : 
-        $('#quantity').val(value - 1);
+$('.decrement').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue - 1);
+   var itemId = $(this).data('item_id');
+
+    isNaN(currentValue) || currentValue <= 0 ? 
+        $('.quantity').val(0) : 
+        $('.quantity').val(currentValue - 1);
 });
